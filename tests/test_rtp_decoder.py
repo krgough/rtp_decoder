7ec2892ebeb2eed317477fe7804934c4bb9c8035
krgough <krgough@gmail.com>
2021-12-31 14:51:42 +0000

    updates

--- tests/test_rtp_decoder.py
+++ tests/test_rtp_decoder.py
@@ -1,9 +1,11 @@
 """
 Tests for rtp_decoder
 """
+
+import pytest
+
 from rtp_decoder import __version__
 from rtp_decoder import rtp_packets as rtp
-# import pytest
 
 
 def test_version():
@@ -12,7 +14,31 @@ def test_version():
     assert __version__ == '0.1.0'
 
 
-def test_rtp_code_decode():
+test_packets = [
+    # Simple packet - no extensions
+    ('8008d4340000303c0b12671a'
+     'd5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5'
+     'd5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5'
+     'd5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5'
+     'd5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5'
+     'd5d5d5d5d5d5d5d5'),
+
+    # Packet with extensions
+    # 88 = Byte 1, Bit 3 set means extension(s) in packets
+    # alba = Byte 12/13 = extension 'name'
+    # deadbeaf = bytes 14-17 = 1st 32bit extension
+    # cab00d1e = bytes 18-21 = 2nd 32bit extension
+    ('8808d4340000303c0b12671aalba0002deadbeafcab00d1e'
+     'd5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5'
+     'd5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5'
+     'd5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5'
+     'd5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5'
+     'd5d5d5d5d5d5d5d5'),
+]
+
+
+@pytest.mark.parametrize("test_data", test_packets)
+def test_rtp_code_decode(test_data):
     """ With the given raw rtp bytes,
         decode those bytes to get the fields and data
         Recontruct packet with that data and confirm it
@@ -25,6 +51,7 @@ def test_rtp_code_decode():
                     'd5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d'
                     '5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5d5'
                     'd5d5d5d5d5d5d5')
+    packet_bytes = test_data
     d_pkt = rtp.decode_rtp_packet(packet_bytes)
     # print(d_pkt)
 
